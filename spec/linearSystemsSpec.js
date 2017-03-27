35122c12dfc811d34ea1e9a1e1f1f7f7a76f4f56
dylan <dylan.kojicheslin@gmail.com>
2017-03-27 02:01:29 -0600

    _getRowScaledBy describe/mutation check

--- spec/linearSystemsSpec.js
+++ spec/linearSystemsSpec.js
@@ -22,17 +22,22 @@ describe('linearSystem', () => {
         expect(data).toEqual({ s : [[2,2,6],[9,9,30]]});
       });
     });
+    describe("_getRowScaledBy", () => {
+      it('_getRowScaledBy should return array mulitplied by scale', () => {
+        let row = linSys._getRowScaledBy([2,0,6],2);
+        expect(row).toEqual([4,0,12]);
+      });
 
+      it('_getRowScaledBy should return array mulitplied to negitive scale', () => {
+        let row = linSys._getRowScaledBy([2,0,-6],-1);
+        expect(row).toEqual([-2,0,6]);
+      });
 
-  it('linearSystem._getRowScaledBy should return array mulitplied by scale', () => {
-    let row = linSys._getRowScaledBy([2,0,6],2);
-    expect(row).toEqual([4,0,12]);
-  });
-
-  it('linearSystem._getRowScaledBy should return array mulitplied to negitive scale', () => {
-    let row = linSys._getRowScaledBy([2,0,-6],-1);
-    expect(row).toEqual([-2,0,6]);
-  });
+      it('_getRowScaledBy mutation check', () => {
+        let row = linSys._getRowScaledBy([2,0,-6],-1);
+        expect(data).toEqual({s : [[2,2,6],[9,9,30]]});
+      });
+    });
 
   it('_rowReplacement should call _getRowScaledBy with data', () => {
     let scaledBy = sinon.spy(linSys, '_getRowScaledBy');
