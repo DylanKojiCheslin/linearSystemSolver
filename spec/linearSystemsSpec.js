b3e727d589ab892da07e89949f4802e1fa47cafd
dylan <dylan.kojicheslin@gmail.com>
2018-06-03 23:11:09 -0600

    cruft

--- spec/linearSystemsSpec.js
+++ spec/linearSystemsSpec.js
@@ -1,3 +1,36 @@
+// import LinearSystem from '/index.js'
+//
+// const initObject = [
+//   [2,3,0,5],
+//   [0,0,1,20],
+//   [0,-2,0,5]
+// ];
+//
+// const theSystem = new LinearSystem(initObject);
+// const solution = theSystem.solve();
+
+// const nullTestinit = null;
+//
+// const nullTestSystem = new LinearSystem(nullTestinit);
+
+// const wrongShapeInit = [
+//   [2,3,0,5],
+//   [0,0,1],
+//   [0,-2,0,5]
+// ];
+//
+// const wrongShapeTestSystem = new LinearSystem(wrongShapeInit);
+
+// const emptyInitObject = [
+//   [0,0,0,0],
+//   [0,0,0,0],
+//   [0,0,0,0]
+// ];
+//
+// const theEmptySystem = new LinearSystem(emptyInitObject);
+// theEmptySystem.solve();
+
+
 // import linearSystem from '../index.js'
 // import sinon from 'sinon'
 // describe('linearSystem', () => {
